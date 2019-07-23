077c8da8457f82bc74dc9c9a5e51bcc039455347
Michael.Lee <mygosity@gmail.com>
2019-07-23 21:16:26 +1000

    refactored to module.exports and almost vanilla js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 let registeredEvents = {};
 
-class EventControl {
+function EventControl() {
   /**
    * Calling this will force all callbacks listening to the eventId to fire
    * along with any arguments you've specified in both the addEventListener event and this one
    * @param {string} eventId a unique string which triggers all callbacks listening to the id
    * @param  {...any} args optional arguments that are added to the callback arguments
    */
-  emit(eventId, ...args) {
+  this.emit = function(eventId, ...args) {
     const eventList = registeredEvents[eventId];
     if (eventList !== undefined) {
       for (let i = 0; i < eventList.length; ++i) {
         eventList[i].event(...args);
       }
     }
-  }
+  };
 
   /**
    * Add an event listener to dispatch from anywhere within the application
@@ -23,12 +23,7 @@ class EventControl {
    * @param {object} ctx an optional reference to bind 'this' to when the callback is fired
    * @param {...any} args additional optional arguments to pass to the callback
    */
-  add(eventId, callback, ctx, ...args) {
-    if (callback === undefined) {
-      // eslint-disable-next-line
-      console.log("EventControl::addEventListener:: callback is undefined!");
-      return;
-    }
+  this.add = function(eventId, callback, ctx, ...args) {
     if (registeredEvents[eventId] === undefined) {
       registeredEvents[eventId] = [];
     }
@@ -39,14 +34,14 @@ class EventControl {
       //stored for reference removal in remove
       callback
     });
-  }
+  };
 
   /**
    * Remove a callback from listening to a particular event id
    * @param {string} eventId a unique string which triggers the callback
    * @param {function} callback function that is called when the event id is dispatched
    */
-  remove(eventId, callback) {
+  this.remove = function(eventId, callback) {
     const eventList = registeredEvents[eventId];
     if (eventList !== undefined) {
       for (let i = eventList.length - 1; i >= 0; --i) {
@@ -58,14 +53,16 @@ class EventControl {
         delete registeredEvents[eventId];
       }
     }
-  }
+  };
 
-  dispose() {
+  this.dispose = function() {
     for (let eventId in registeredEvents) {
       registeredEvents[eventId] = null;
     }
     registeredEvents = {};
-  }
+  };
 }
 
-export default new EventControl();
+const eventcontrol = new EventControl();
+module.exports = eventcontrol;
+module.exports.default = eventcontrol;
